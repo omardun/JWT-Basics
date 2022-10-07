093a42a14bfa7212a5dde8d3b394b720b0901fe3
omardun <omar.rosquero10@gmail.com>
2022-10-06 22:28:43 -0400

    user token if account is created

--- controllers/main.js
+++ controllers/main.js
@@ -1,47 +1,34 @@
 const jwt = require('jsonwebtoken')
 const CustomAPIError = require('../errors/custom-error')
 
-const login = async (req,res) => {
-    const {username,password} = req.body
+const login = async (req, res) => {
+    const { username, password } = req.body
     // console.log(username,password);
 
-if(!username || !password) {
- throw new CustomAPIError('please provide email and password', 400)
-}
-//jsut for demo, normally provided DB!!!
-const id = new Date().getDate()
-
-
-//try to keep payload small, better experience for user
-//just for demo, in production use long, complex and unguessable string value!!!
-const token = jwt.sign({id,username}, process.env.JWT_SECRET,{expiresIn:'30d'})
-
-    res.status(200).json({msg: 'user created', token })
-}
-
-const dashboard = async (req,res) => {
-    const authHeader = req.headers.authorization;
-    if(!authHeader || !authHeader.startsWith('Bearer ')) {
-        throw new CustomAPIError('No token provided', 401)
+    if (!username || !password) {
+        throw new CustomAPIError('please provide email and password', 400)
     }
+    //jsut for demo, normally provided DB!!!
+    const id = new Date().getDate()
 
-    const token = authHeader.split(' ')[1]
 
-    try {
-const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    //try to keep payload small, better experience for user
+    //just for demo, in production use long, complex and unguessable string value!!!
+    const token = jwt.sign({ id, username }, process.env.JWT_SECRET, { expiresIn: '30d' })
 
-const luckyNumber = Math.floor(Math.random()*100)
+    res.status(200).json({ msg: 'user created', token })
+}
 
-    res.status(200).json({msg: `Hello, ${decoded.username}`, secret:`Here is your authorized data, your lucky number is ${luckyNumber}`
-})
-    }catch(error) {
-        throw new CustomAPIError('Not authorized to access this route', 401)
+const dashboard = async (req, res) => {
+    console.log(req.user);
+    const luckyNumber = Math.floor(Math.random() * 100)
 
-    }
+    res.status(200).json({
+        msg: `Hello, ${req.user.username}`, secret: `Here is your authorized data, your lucky number is ${luckyNumber}`
+    })
 
-    
 }
 
 module.exports = {
-    login,dashboard
+    login, dashboard
 }
